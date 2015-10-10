c277e3720603735f2df4c6767efb8b833baf98b7
Spencer Florence <florence@ccs.neu.edu>
2015-10-10 08:56:05 -0500

    fix introducing an unbound begin-for-syntax

--- errortrace-test/tests/errortrace/begin.rkt
+++ errortrace-test/tests/errortrace/begin.rkt
@@ -6,9 +6,14 @@
   (define ns (make-base-namespace))
   (parameterize ([current-namespace ns])
     (dynamic-require 'errortrace #f)
-    (eval '(module m racket/base 
-             (require (for-syntax racket/base))
-             (begin-for-syntax 1 2)))))
+    (eval '(module m racket/base
+             (require (for-syntax racket/base)
+                      (for-meta 2 racket/base)
+                      (for-meta 3 racket/base))
+             (begin-for-syntax
+               1 2
+               (begin-for-syntax 1
+                                 (begin-for-syntax 2)))))))
 
 (module+ main
   (begin-for-syntax-tests))
