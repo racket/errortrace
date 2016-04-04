c850a6cc7b1ab4ee62cb5ca812ab4e5ab7ab458f
Spencer Florence <spencer@florence.io>
2016-04-04 12:14:45 -0500

    fix let coverage of lhs

--- errortrace-test/tests/errortrace/main.rkt
+++ errortrace-test/tests/errortrace/main.rkt
@@ -5,11 +5,13 @@
          "alert.rkt"
          "phase-1.rkt"
          "phase-1-eval.rkt"
-         "begin.rkt")
+         "begin.rkt"
+         "coverage-let.rkt")
 
 (wrap-tests)
 
 (test do (alert-tests))
+(test do (letrec-test))
 
 (phase-1-tests)
 (phase-1-eval-tests)
