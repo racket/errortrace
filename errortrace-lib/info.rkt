782c7bfe591076d71c067928a85cae893df26580
Alex Knauth <alexander@knauth.org>
2016-08-19 10:18:05 -0400

    use `lang-reader-module-paths` instead of copy-pasted code
    Relies on https://github.com/racket/racket/commit/42dcc525b1e03ca36488b815b45428655fdcbf1b

--- errortrace-lib/info.rkt
+++ errortrace-lib/info.rkt
@@ -1,6 +1,6 @@
 #lang info
 (define collection 'multi)
-(define deps '("base" "source-syntax"))
+(define deps '(["base" #:version "6.6.0.3"] "source-syntax"))
 
 (define pkg-desc "implementation (no documentation) part of \"errortrace\"")
 
