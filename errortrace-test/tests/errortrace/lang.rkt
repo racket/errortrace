9690fa5fd9888e9e244eb311994dcd8884ffb7a9
Sorawee Porncharoenwase <sorawee.pwase@gmail.com>
2022-06-05 08:51:07 -0700

    Add tests

--- errortrace-test/tests/errortrace/lang.rkt
+++ errortrace-test/tests/errortrace/lang.rkt
@@ -1,5 +1,7 @@
 #lang errortrace racket/base
 
+(require racket/string)
+
 (define (f x)
   (/ (+ 1 x) 10))
 
@@ -11,5 +13,15 @@
 (parameterize ([current-error-port o])
   ((error-display-handler) (exn-message failed) failed))
 
-(unless (regexp-match? #rx"errortrace...:\n[^\n]*[(][+] 1 x[)]" (get-output-bytes o))
+(unless (regexp-match? #rx"errortrace\\.\\.\\.:\n[^\n]*[(][+] 1 x[)]" (get-output-bytes o))
   (error "expected expression not in the error output"))
+
+(unless (string-prefix? (get-output-string o) "+: contract violation\n  expected: number?\n  given: 'bad\n  errortrace...:\n")
+  (error "unexpected format (without custom msg)"))
+
+(define o2 (open-output-bytes))
+(parameterize ([current-error-port o2])
+  ((error-display-handler) "foobar" failed))
+
+(unless (string-prefix? (get-output-string o2) "foobar\n  message: +: contract violation\n  expected: number?\n  given: 'bad\n  errortrace...:\n")
+  (error "unexpected format (with custom msg)"))
