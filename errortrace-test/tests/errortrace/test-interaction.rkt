6fb1bd2a1c2de8af52d2bc85d096347381d17fee
Matthew Flatt <mflatt@racket-lang.org>
2022-10-04 06:25:09 +0800

    Revert "Make #lang errortrace support REPL"
    This reverts commit 574fd7ec038357a4475561df15e67f7594926f6a.
    
    The change didn't work out as intended, because it can end up changing
    the `current-compile` handler in a compilation context such as `raco
    setup`, which has a bad effect as compilation continues to other
    modules.

--- errortrace-test/tests/errortrace/test-interaction.rkt
+++
@@ -1,8 +0,0 @@
-#lang racket
-
-(require rackunit)
-
-(require "lang.rkt")
-
-(parameterize ([current-namespace (module->namespace "lang.rkt")])
-  (check-equal? (eval '(#%top-interaction . (f 0))) 1/10))
